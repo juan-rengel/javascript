0ed060fc279e99d61881e91aba87670568e5da03
juan rengel <110213054+juan-rengel@users.noreply.github.com>
2022-09-05 20:32:02 -0400

    atualize javascript

---
+++ condiçao-if/exerc01.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log('vivendo em '+ pais)
+if(pais == 'brasil'){
+    console.log('Brasileiro')
+}else{
+    console.log('extrangeiro')
+}
---
+++ condiçao-if/exerc02.js
@@ -0,0 +1,10 @@
+//CONDÇAO SIMPLES
+//nao tem else if 
+
+var vel = 50
+console.log('sua velocidade é ' + vel + 'km/h')
+
+if(vel > 60){
+    console.log('voce ultrapaso o limite de velocidade foi MULTADO')
+}
+console.log('dirija sempre com cinto de segurança')
