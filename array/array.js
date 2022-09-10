e61e968c8cd850d726d0d0971336fcfc3fecef22
juan rengel <110213054+juan-rengel@users.noreply.github.com>
2022-09-09 23:01:59 -0400

    atualizaçao javascript

---
+++ array/array.js
@@ -0,0 +1,26 @@
+var aluno = new Array('juan','jose','linares','rengel')
+console.log(aluno[2])
+
+var alunos = ['perez','sebastian','joao','mario','jimenez']
+
+for(var i = 0; i < alunos.length; i++){
+    console.log(alunos[i])
+}
+
+var suma = [100,200,300,400]
+
+for(var n = 0; n < suma.length; n++){
+    console.log(suma[n])
+}
+
+/*
+ o array: é um elemento que tem um conjunto de variavel 
+ outra meneira de usar o array junto com for é so seleccionando o in '0'. E o of 'elemento' 
+
+/* for (var i  in alunos){
+    console.log(i)
+}
+
+    for (var i of alunos){
+        console.log(i)
+    }
